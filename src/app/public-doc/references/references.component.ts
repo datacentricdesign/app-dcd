2180a96feb1560035a4728b30177b8aac95505b8
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2022-06-07 08:24:25 +0200

    added swagger ui for bucket api on reference page

--- src/app/public-doc/references/references.component.ts
+++ src/app/public-doc/references/references.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
+import { SwaggerUIBundle } from 'swagger-ui-dist';
+
 @Component({
     selector: 'app-references-cmp',
     moduleId: module.id,
@@ -14,5 +16,43 @@ export class ReferencesComponent implements OnInit {
 
     ngOnInit() {
         this.titleService.setTitle("References - DCD Lab");
+
+        // const MyCustomPlugin = function() {
+        //     return {
+        //       wrapComponents: {
+        //         // add text above InfoContainer - same effect as above title
+        //         InfoContainer: (Original, { React }) => (props) => {
+        //             console.log(props)
+        //           return React.createElement("div", null,
+        //             React.createElement("h3", null, "I'm above the InfoContainer"),
+        //             React.createElement(Original, props)
+        //           )
+        //         },
+        
+        //         // and/or add text above API description
+        //         InfoUrl: (Original, { React }) => (props) => {
+        //           return React.createElement("div", null,
+        //             React.createElement(Original, props),
+        //             React.createElement("h3", null, "I'm above the API description")
+        //           )
+        //         },
+
+        //         Info: () => null
+        //       }
+        //     }
+        //   }
+
+        const ui = SwaggerUIBundle({
+            dom_id: '#swagger-ui-bucket',
+            layout: 'BaseLayout',
+            // plugins: [
+            //     MyCustomPlugin
+            //   ],
+            presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIBundle.SwaggerUIStandalonePreset
+            ],
+            url: 'https://raw.githubusercontent.com/datacentricdesign/bucket/master/openapi/openapi.json'
+          });
     }
 }
